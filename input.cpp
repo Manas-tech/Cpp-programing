794f2c36ecb5b4c36d7f17eb61eb9a251bc23f96
manas parwani _TECHNONJR <115521778+Manas-tech@users.noreply.github.com>
2023-01-12 12:52:00 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,20 @@
+// C++ program to demonstrate the
+// cin object
+#include <iostream>
+using namespace std;
+  
+// Driver Code
+int main()
+{
+    int i;
+  
+    // Take input using cin
+    std::cout << "ENTER THE INPUT ";
+    cin >> i;
+
+  std::cout << "THE INPUT IS ";
+    // Print output
+    cout << i;
+  
+    return 0;
+}
