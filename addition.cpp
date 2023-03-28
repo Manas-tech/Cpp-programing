7c47ebfae376481c661db85842ef51dbb018d4a4
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-03-28 23:14:25 +0530

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -9,7 +9,7 @@ int main() {
   cin >> first_number >> second_number;
 
   // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number;
+  sum = first_number +  second_number;
 
   // prints sum 
   cout << first_number << " + " <<  second_number << " = " << sum;     
