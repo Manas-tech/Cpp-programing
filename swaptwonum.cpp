83fb00e1a2255476bd505520a90e986f513bcaa0
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-04-26 22:00:44 +0530

    Update swaptwonum.cpp

--- swaptwonum.cpp
+++ swaptwonum.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int a = 5, b = 10, temp;
 
-    cout << "Before swapping." << endl;
+    cout << "Before  swapping." << endl;
     cout << "a = " << a << ", b = " << b << endl;
 
     temp = a;
