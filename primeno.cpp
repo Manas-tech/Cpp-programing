8dc3a9d2b6b3da8490524c032470e0154479a2db
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-05-20 22:54:03 +0530

    Update primeno.cpp

--- primeno.cpp
+++ primeno.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()  
 {  
   int n, i, m=0, flag=0;  
-  cout << "Enter the Number to check Prime: ";  
+  cout << "Enter the Number  to check Prime: ";  
   cin >> n;  
   m=n/2;  
   for(i = 2; i <= m; i++)  
