f9649b3d16b60599df9541b01944ee3b3118cae1
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-08-25 18:50:06 +0530

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-  int first_number,   second_number, sum;
+  int first_number,second_number, sum;
     
   cout << "Enter two numbers:";
   cin >> first_number >> second_number;
