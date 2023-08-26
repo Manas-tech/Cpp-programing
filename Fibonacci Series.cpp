6555bc2f1855cda5fecd49e2a757555d83d201e8
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-08-26 20:42:11 +0530

    Update Fibonacci Series.cpp

--- Fibonacci Series.cpp
+++ Fibonacci Series.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n, t1 = 0, t2 = 1, nextTerm = 0;
 
-    cout << "Enter the number of  terms: ";
+    cout << "Enter the number of terms: ";
     cin >> n;
 
     cout << "Fibonacci  Series: ";
