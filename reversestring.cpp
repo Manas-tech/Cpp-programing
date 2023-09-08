9c0f718df2a09fdb4f01383694f60241047a9954
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-09-08 22:54:30 +0530

    Update reversestring.cpp

--- reversestring.cpp
+++ reversestring.cpp
@@ -12,7 +12,7 @@ void reverseStr(string& str)
 	// Swap character   starting from two
 	// corners
 	for (int i = 0; i < n / 2; i++)
-		swap(str[i], str[n - i - 1]);
+		swap(str[i],str[n - i - 1]);
 }
 
 // Driver program
